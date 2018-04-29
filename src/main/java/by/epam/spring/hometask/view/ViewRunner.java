2df6ccb505795fbb828fe7c61e5497861e23edac
vekony <norbert.vekony@gmail.com>
2018-04-29 11:44:19 +0200

    integrate spring boot

--- src/main/java/by/epam/spring/hometask/view/ViewRunner.java
+++ src/main/java/by/epam/spring/hometask/view/ViewRunner.java
@@ -1,17 +1,26 @@
 package by.epam.spring.hometask.view;
 
-import by.epam.spring.hometask.controller.configuration.AppConfig;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@SpringBootApplication
+@ComponentScan(basePackages = "by.epam.spring.hometask")
 public class ViewRunner {
 
     public static void main(String[] args) {
+        SpringApplication.run(ViewRunner.class, args);
+    }
 
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ctx.register(AppConfig.class);
-        ctx.refresh();
-        ViewPerformer performer = ctx.getBean("viewPerformer", ViewPerformer.class);
-        performer.openConsoleSession();
-        ctx.close();
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            ViewPerformer performer = ctx.getBean("viewPerformer", ViewPerformer.class);
+            performer.openConsoleSession();
+        };
     }
 }
